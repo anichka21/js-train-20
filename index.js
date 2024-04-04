297043db55b5961ffb6838ad90e47b123f65fcd1
anichka21 <anashnaidruk@gmail.com>
2024-04-04 14:36:28 +0300

    js-train-20

--- index.js
+++ index.js
@@ -21,12 +21,12 @@ function Vehicle(brand, model, year, mileage) {
 
 // Рядковому представленю Vehicle призначаємо функцію яка повертає рядок: <brand> <model> <year>
 
-this.toString = function() {
+Vehicle.toString = function() {
   return `${this.brand} ${this.model} ${this.year}`;
 };
 // valueOf - це метод, який використовується JavaScript для конвертації об'єкта в примітивне значення.
 // Ми перевизначаємо його тут, щоб він повертав this.mileage.
-this.valueOf = function() {
+Vehicle.valueOf = function() {
   return this.mileage;
 };
 
